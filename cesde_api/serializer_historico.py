ce6e6ab14241caee5db87ad802dae32ddf5aa6dd
JeffersonMontoya <chechemontoya1997@gmail.com>
2024-08-09 10:45:01 -0500

    09/08/2024

--- cesde_api/serializer_historico.py
+++ cesde_api/serializer_historico.py
@@ -1,31 +1,17 @@
+
 from rest_framework import serializers
 from .models import Gestiones
 
-class HistoricoSerializer(serializers.ModelSerializer):
-    fechas_gestiones_por_aspirante = serializers.SerializerMethodField()
+class HistoricoGestionesSerializer(serializers.ModelSerializer):
+    tipo_gestion_nombre = serializers.SerializerMethodField()
+    tipificacion_nombre = serializers.SerializerMethodField()
 
     class Meta:
         model = Gestiones
-        fields = ['fechas_gestiones_por_aspirante']
+        fields = ['id', 'cel_aspirante', 'fecha', 'tipo_gestion', 'tipo_gestion_nombre', 'observaciones', 'tipificacion', 'tipificacion_nombre', 'asesor']
 
-    def get_fechas_gestiones_por_aspirante(self, obj):
-        celular = self.context['request'].query_params.get('celular_aspirante')
-        if celular:
-            gestiones = Gestiones.objects.filter(cel_aspirante__celular=celular).order_by('fecha')
-        else:
-            gestiones = Gestiones.objects.none()  # Retornar vacío si no se proporciona celular
+    def get_tipo_gestion_nombre(self, obj):
+        return obj.tipo_gestion.nombre if obj.tipo_gestion else None
 
-        return [
-            {
-                'nombre_completo': f"{gestion.cel_aspirante.nombre} {gestion.cel_aspirante.apellidos}",
-                'celular': gestion.cel_aspirante.celular,
-                'fecha': gestion.fecha,
-                'asesor': gestion.asesor.nombre_completo,
-                'descripcion': gestion.observaciones,
-                'tipo_gestion': gestion.tipo_gestion.nombre,
-                'resultado_gestion': gestion.tipificacion.nombre,
-                'programa': gestion.cel_aspirante.programa.nombre,
-                'sede': gestion.cel_aspirante.sede.nombre
-            }
-            for gestion in gestiones
-        ]
+    def get_tipificacion_nombre(self, obj):
+        return obj.tipificacion.nombre if obj.tipificacion else None
