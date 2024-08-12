f048bfcfccc8d0dd38c56fa059a09d56dabefdba
Sebastian SD. Duque Salazar <1193485552@andesbpo.local>
2024-08-12 14:29:28 -0500

    traer

--- cesde/settings.py
+++ cesde/settings.py
@@ -139,6 +139,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "http://localhost:5173",
+
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
