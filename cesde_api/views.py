76a50d83ac48c5a4116d1b4d0b5eff7ef2b51398
JeffersonMontoya <chechemontoya1997@gmail.com>
2024-08-20 11:55:32 -0500

    tercera version

--- cesde_api/views.py
+++ cesde_api/views.py
@@ -35,6 +35,18 @@ class CustomPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100  # Tamaño máximo de página permitido
 
+    def get_paginated_response(self, data):
+        """
+        Devuelve una respuesta paginada que incluye la información de paginación.
+        """
+        return Response({
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'page_size': self.page.paginator.per_page,
+            'results': data
+        })
+
 
 class SedeViewSet(viewsets.ModelViewSet):
     queryset = Sede.objects.all()
@@ -252,7 +264,6 @@ class FilterProcesosViewSet(viewsets.ViewSet):
 
         # Devuelve la respuesta paginada
         return paginator.get_paginated_response(serializer.data)
-
 
 
 class EstadisticasViewSet(viewsets.GenericViewSet):
