4079e046dc12b5707b5e87eba2c6d70966e93fb7
Alexisg22 <138946234+Alexisg22@users.noreply.github.com>
2024-09-04 08:57:28 -0500

    Update apps.py

--- cesde_api/apps.py
+++ cesde_api/apps.py
@@ -4,6 +4,3 @@ class CesdeApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cesde_api'
     
-    def ready(self):
-        # Importa el archivo de señales para que las señales se registren
-        import cesde_api.signals
